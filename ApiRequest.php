6990d38176e385e77e6ae32ea99f8af0d8b1a609
Idriss Boukmouche <69165137+TerminalDZ@users.noreply.github.com>
2024-04-17 13:26:37 +0100

    Update ApiRequest.php

--- ApiRequest.php
+++ ApiRequest.php
@@ -106,36 +106,6 @@ class ApiRequest {
 
 
 
-public function UploadFile($endpoint, $filePath)
-{
-    $curl = curl_init();
-    $file = new CURLFile($filePath);
-
-    curl_setopt_array($curl, array(
-        CURLOPT_URL => $this->url . $endpoint,
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => "",
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 0,
-        CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => "POST",
-        CURLOPT_POSTFIELDS => array(
-            'file' => $file
-        ),
-        CURLOPT_HTTPHEADER => array(
-            "Content-Type: multipart/form-data"
-        ),
-    ));
-
-    $response = curl_exec($curl);
-
-    curl_close($curl);
-
-    return json_decode($response);
-}
-
-
 
 
 
