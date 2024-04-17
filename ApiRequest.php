ca3515fe364bfc5b7b1e84f060df0c794d14aefb
Idriss Boukmouche <69165137+TerminalDZ@users.noreply.github.com>
2024-04-17 13:17:55 +0100

    Update ApiRequest.php

--- ApiRequest.php
+++ ApiRequest.php
@@ -106,29 +106,34 @@ class ApiRequest {
 
 
 
-    public function UploadFile($endpoint, $data) {
-        $curl = curl_init();
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => $this->url.$endpoint,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "POST",
-            CURLOPT_POSTFIELDS => $data,
-            CURLOPT_HTTPHEADER => array(
-                "Content-Type: multipart/form-data"
-            ),
-        ));
+public function UploadFile($endpoint, $filePath)
+{
+    $curl = curl_init();
+    $file = new CURLFile($filePath);
 
-        $response = curl_exec($curl);
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => $this->url . $endpoint,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "POST",
+        CURLOPT_POSTFIELDS => array(
+            'file' => $file
+        ),
+        CURLOPT_HTTPHEADER => array(
+            "Content-Type: multipart/form-data"
+        ),
+    ));
 
-        curl_close($curl);
+    $response = curl_exec($curl);
 
-        return json_decode($response);
-    }
+    curl_close($curl);
+
+    return json_decode($response);
+}
 
 
 
